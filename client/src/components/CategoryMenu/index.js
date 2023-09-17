5d9e69632235931adc115e17551a21f45bdc7987
c0nstantin1990 <118696575+c0nstantin1990@users.noreply.github.com>
2023-09-17 12:29:39 -0400

    categotymenu using redux

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useQuery } from "@apollo/client";
-import { useStoreContext } from "../../utils/GlobalState";
+import { useDispatch, useSelector } from "react-redux";
 import {
   UPDATE_CATEGORIES,
   UPDATE_CURRENT_CATEGORY,
@@ -8,8 +8,11 @@ import {
 import { QUERY_CATEGORIES } from "../../utils/queries";
 import { idbPromise } from "../../utils/helpers";
 
-function CategoryMenu() {
-  const [state, dispatch] = useStoreContext();
+function CategoryMenu({}) {
+  const state = useSelector((state) => {
+    return state;
+  });
+  const dispatch = useDispatch();
 
   const { categories } = state;
 
